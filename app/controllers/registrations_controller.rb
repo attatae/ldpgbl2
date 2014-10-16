024112b97dacc06ca8defe4f2b36a034c4688342
Cuong <cuongnm17290@gmail.com>
2014-10-16 15:41:51 +0700

    refine city input style when signup

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
   def create
+    binding.pry
     @user = User.new user_params
     if @user.save
       UserMailer.send_confirm_email(@user).deliver
